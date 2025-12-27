90921be6c930ba4e3a7f08e9c38ac4bfa5539938
👿☠️CHINMAYA☠️👿 <chinmaya@Buntys-MacBook.local>
2025-12-28 01:51:06 +0530

    Spelling

--- src/components/CategoryBar.jsx
+++ src/components/CategoryBar.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { SearchContext } from "../context/SearchContext";
 export default function CategoryBar(){
   const {setCategory}=useContext(SearchContext);
-  const categories=["all","electronics","jewelery","men's clothing","women's clothing"];
+  const categories=["all","electronics","jwellery","men's clothing","women's clothing"];
   return <div className="d-flex justify-content-center gap-3 mt-3 category">
     {categories.map(c=> <span key={c} onClick={()=>setCategory(c)}>{c.charAt(0).toUpperCase()+c.slice(1)}</span>)}
   </div>
